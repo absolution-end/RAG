a459e4133b182c5ba3a0d8e59e5cb7b26055c0e3
Chirayu <chirayu47sharma@gmail.com>
2025-09-15 10:23:56 +0530

    Starting again

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
+# Load API
+# Set up local embedding with "SentenceTransformer"
+# Create a persistant Chroma collection to store and search docs.
+# connect to OpenRouter
+# Ask a model a question
+# print the answer
+
 import os
 from dotenv import load_dotenv
 import chromadb
@@ -6,8 +13,8 @@ from chromadb.utils import embedding_functions
 
 # load environment variable from .env file
 load_dotenv()
-os.environ["OPENAI_API_BASE"] = "https://openrouter.ai/api/v1"
-openai_key = os.getenv("OPENAI_API_KEY")
+os.environ["OPENROUTER_API_BASE"] = "https://openrouter.ai/api/v1"
+openrouter_key = os.getenv("OPENROUTER_API_KEY")
 
 local_ef = embedding_functions.SentenceTransformerEmbeddingFunction( 
                                                         model_name= "all-MiniLM-L6-v2")
@@ -20,7 +27,7 @@ collection = chroma_client.get_or_create_collection(
     embedding_function=local_ef
     )
 
-client = OpenAI(api_key=openai_key, base_url="https://openrouter.ai/api/v1")
+client = OpenAI(api_key=openrouter_key, base_url="https://openrouter.ai/api/v1")
 
 resp = client.chat.completions.create(
     model="mistralai/mistral-7b-instruct",
